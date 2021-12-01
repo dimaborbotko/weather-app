9776e0bbb8c711f5338ac6a344ef1c61e1817bea
Dima Borbotko <68921364+dimashechka@users.noreply.github.com>
2021-12-01 14:24:15 +0200

    todays-weather-data-&-ui-top-part

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import AppLoading from 'expo-app-loading';
+import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
+import Weather from "./components/Weather";
 
 const fonts = async () => {
   await Font.loadAsync({
@@ -14,15 +15,55 @@ const fonts = async () => {
   });
 };
 
+const API_KEY = "526d4889d65b48cabba160455213011";
+
 export default function App() {
   const [font, setFont] = useState(false);
+
+  const [weatherData, setWeatherData] = useState(null);
+  const [loaded, setLoaded] = useState(true);
+
+  async function getWeather(cityName) {
+    setLoaded(false);
+    const API = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${cityName}&aqi=yes`;
+    try {
+      const response = await fetch(API);
+      if (response.status === 200) {
+        const data = await response.json();
+        setWeatherData(data);
+      } else {
+        setWeatherData(null);
+      }
+      setLoaded(true);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getWeather("New York");
+  }, []);
+
+  if (!loaded) {
+    return (
+      <LinearGradient style={styles.loadingCont} colors={["#7383f2", "#a4caf5"]}>
+        <ActivityIndicator color="#6f5dee" size={58} />
+      </LinearGradient>
+    );
+  } else if (weatherData === null) {
+    return <View></View>;
+  }
 
   if (font) {
     return (
       <LinearGradient style={styles.container} colors={["#7383f2", "#a4caf5"]}>
-        <View style={styles.header}>
-          <Text style={styles.title}>Weather App</Text>
+        <View style={styles.header} >
+          <Weather weatherData={weatherData} />
         </View>
+        <View style={styles.oldWeather}>
+
+        </View>
+        
       </LinearGradient>
     );
   } else {
@@ -40,13 +81,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  header: {
+  loadingCont: {
+    flex: 1,
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: "center"
+  },
+  header: {
+    flex: 2,
     marginTop: 40,
   },
-  title: {
-    fontSize: 25,
-    fontFamily: 'pnb',
-  },
+  oldWeather: {
+    flex: 3
+  }
 });
---
+++ components/Weather.js
@@ -0,0 +1,88 @@
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+
+export default function Weather({ weatherData }) {
+  const { current, location } = weatherData;
+  const { temp_c, feelslike_c, condition, precip_mm } = current; // weather info
+  const { name, localtime } = location; // location info
+  const { text, icon } = condition; // sky condition info
+
+  useEffect(() => {
+    console.log(temp_c);
+  }, [weatherData]);
+
+  const locIcon = <Entypo name="location-pin" size={20} color="white" />;
+  return (
+    <View style={styles.container}>
+      <View style={styles.mainBlock}>
+        <View style={styles.timeInfo}>
+          <Text style={styles.city}>{locIcon}{name}</Text>
+          <Text style={styles.date}>{localtime}</Text>
+        </View>
+        <View style={styles.tempInfo}>
+            <Image style={styles.imgWeather} source={{uri: `http:${icon}` }}/>
+            <Text style={styles.temp}>{temp_c}°</Text>
+        </View>
+        
+        <Text style={styles.tempFeel}>Feels like {feelslike_c}°</Text>
+        <Text style={styles.skyCond}>{text}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  mainBlock: {
+    alignItems: "center",
+    flex: 2,
+    justifyContent: "space-evenly",
+  },
+  timeInfo: {
+    alignItems: "center"
+  },
+  city: {
+    color: "white",
+    fontFamily: "pnsb",
+    fontSize: 20,
+    marginBottom: 8
+  },
+  date: {
+      color: '#bfbfbf',
+      fontFamily: 'pnr'
+  },
+
+  tempInfo: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  imgWeather: {
+      width: 75,
+      height: 75,
+  },
+  temp: {
+      fontFamily: 'pnr',
+      fontSize: 75,
+      color: 'white'
+  },
+  spanDeg: {
+    fontSize: 50,
+    backgroundColor: 'red'
+  },
+  tempFeel: {
+    color: "white",
+    fontFamily: "pnsb",
+    fontSize: 20,
+    marginBottom: 8
+  },
+  skyCond: {
+    color: "white",
+    fontFamily: "pnsb",
+    fontSize: 20,
+    marginBottom: 8
+  }
+});
